527e71e246179c2216923e7c2254798f6c94c10a
JoeyHere <josephodell@outlook.com>
2019-02-21 11:11:23 +0000

    Done.

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,1 +1,19 @@
-// You can add your event handlers here! 
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM has been fully loaded')
+    console.table(gifts)
+    loadGifts(gifts)
+})
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault()
+    addGift(inputNameEl.value)
+    formEl.reset()
+    }
+)
+
+filterEl.addEventListener('input', () => {
+    const filtered = filterGifts(filterEl.value)
+    loadGifts(filtered)
+}
+)
